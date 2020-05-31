c355134ec722942da3793cc73e4b56184c5bce37
MattyPalka <mateuszpalka90@gmail.com>
2020-05-31 14:53:51 +0100

    final

--- server.js
+++ server.js
@@ -13,12 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
 
-
 app.use('/api', apiRouter)
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
-app.listen(4000)
+
+app.listen(3000)
