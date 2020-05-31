649e065d4a2a33db8e7d8fcc03d25e3e00a0d459
MattyPalka <mateuszpalka90@gmail.com>
2020-05-31 14:22:11 +0100

    added static on server

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+
+
 app.use('/api', apiRouter)
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(4000)
