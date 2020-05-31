dc6a499c7725d0de602c66239832f35649dfc8f9
MattyPalka <mateuszpalka90@gmail.com>
2020-05-31 07:14:17 +0100

    adding data to db

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const db = require('../database/db')
 
 router.post('/storeEvent', async (req, res) => {
-    res.json(req.body)
+    let result = await db.dbCreate('form', {
+        name: req.body.name, surname: req.body.surname, email: req.body.email, date: req.body.date })
+    res.json(result)
 })
 
 module.exports = router
